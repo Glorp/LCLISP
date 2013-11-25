b6aba66e388dbe11c965b24c0ad95ff48311af7f
Glorp <jonas.winje@gmail.com>
2013-11-25 21:31:14 +0100

    things

--- simplestly/example.rkt
+++ simplestly/example.rkt
@@ -1,12 +1,35 @@
 #lang s-exp "lang.rkt"
 
-(def + (λ (a : Nat)
-         (fixn (λ (rec+ : (Nat -> Nat))
-                 (λ (b : Nat)
-                   (ifn (0? b)
-                        (λ (_ : Unit) a)
-                        (λ (_ : Unit) (s (rec+ (p b))))))))))
+(def ->b (λ ((b : Bool) (_ : Unit)) b))
 
-+
+(def ->n (λ ((n : Nat) (_ : Unit)) n))
+
+(def or (λ ((a : Bool) (b : Bool))
+          (ifb a (->b true) (->b b))))
+
+(def pair (λ ((a : Nat) (d : Nat) (b : Bool))
+            (ifn b
+                 (->n a)
+                 (->n d))))
+
+(def fst (λ (p : (Bool -> Nat)) (p true)))
+
+(def snd (λ (p : (Bool -> Nat)) (p false)))
+
+(def 0? (λ (n : Nat)
+          (recb n
+                (λ (_ : Bool) false)
+                true)))
+
+(def + (λ ((a : Nat) (b : Nat)) (recn a s b)))
 
 (+ (s (s 0)) (s (s (s 0))))
+
+(def fib (fixn (λ ((recfib : (Nat -> Nat)) (n : Nat))
+                 (ifn (or (0? n) (0? (p n)))
+                      (->n n)
+                      (λ (_ : Unit) (+ (recfib (p n))
+                                       (recfib (p (p n)))))))))
+
+(fib (+ (s (s (s (s 0)))) (s (s (s (s 0))))))
+
